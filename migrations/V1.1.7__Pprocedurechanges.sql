d966abd5543853f4c1145f4fdb0b49ad307b162a
Bajaj1084 <madhureshbajaj84@gmail.com>
2025-01-03 18:45:54 +0530

    change_procedure

---
+++ migrations/V1.1.7__Pprocedurechanges.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE PROCEDURE insert_data_example(tbl_name STRING, col1_value STRING, col2_value STRING)
+RETURNS STRING
+LANGUAGE SQL
+AS 
+$$
+BEGIN
+    EXECUTE IMMEDIATE 
+        'INSERT INTO ' || tbl_name || 
+        ' VALUES (''' || col1_value || ''', ''' || col2_value || ''')';
+    RETURN 'Row inserted successfully!';
+END;
+$$;
+
+CALL insert_data_example('GIT_INT.DEMO_V3.HELLO_WORLD', 'ABC', 'XYZ');
+

19af6d0f5cb7ed72c0bb056cd114632ee4ee1ca6
Bajaj1084 <madhureshbajaj84@gmail.com>
2025-01-03 19:38:56 +0530

    test

--- migrations/V1.1.5__procedure_v2.sql
+++ migrations/V1.1.5__procedure_v2.sql
@@ -7,17 +7,9 @@ LANGUAGE SQL
 AS
 $$
 BEGIN
-    -- Begin a transaction
-    BEGIN TRANSACTION;
-
-    -- Insert data
     INSERT INTO GIT_INT.DEMO_V3.HELLO_WORLD (FIRST_NAME, LAST_NAME)
     VALUES (:name, :lname);
-
-    -- Commit the transaction
-    COMMIT;
-
-    RETURN 'Row inserted successfully';
+  
 END;
 $$;
 

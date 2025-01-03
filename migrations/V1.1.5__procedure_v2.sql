0f1c8d55ab67123a71cf70ca9e5c90badc36c48e
Bajaj1084 <madhureshbajaj84@gmail.com>
2025-01-03 19:29:50 +0530

    check

--- migrations/V1.1.5__procedure_v2.sql
+++ migrations/V1.1.5__procedure_v2.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE  insert_data_example(
+CREATE OR REPLACE PROCEDURE insert_data_example(
     name STRING,
     lname STRING
 )
@@ -7,9 +7,18 @@ LANGUAGE SQL
 AS
 $$
 BEGIN
-    INSERT INTO GIT_INT.DEMO_V3.HELLO_WORLD (FIRST_NAME , LAST_NAME )
+    -- Begin a transaction
+    BEGIN TRANSACTION;
+
+    -- Insert data
+    INSERT INTO GIT_INT.DEMO_V3.HELLO_WORLD (FIRST_NAME, LAST_NAME)
     VALUES (:name, :lname);
+
+    -- Commit the transaction
+    COMMIT;
+
     RETURN 'Row inserted successfully';
 END;
 $$;
+
 
